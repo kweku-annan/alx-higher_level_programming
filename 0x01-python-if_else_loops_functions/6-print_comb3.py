7ac0f974ab81aa7bdc40048f3f09319a87cd4400
kweku_annan <kwekuannan.dev@gmail.com>
2024-01-17 18:44:52 +0000

    Correction

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,5 +5,5 @@ for i in range(10):
         if (i != 8 or j != 9):
             print("{}{}, ".format(i, j), end="")
         else:
-                  print("{}{}".format(i, j))
+            print("{}{}".format(i, j))
         j += 1
