ea6aa677967e3d10f50030b5829776047263bd17
kweku_annan <kwekuannan.dev@gmail.com>
2024-01-17 18:42:47 +0000

    Add 6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(10):
+    j = i + 1
+    while j <= 9:
+        if (i != 8 or j != 9):
+            print("{}{}, ".format(i, j), end="")
+        else:
+                  print("{}{}".format(i, j))
+        j += 1
