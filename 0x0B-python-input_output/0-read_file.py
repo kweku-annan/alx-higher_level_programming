3ac9ae39d854dffa4d5f0f25e926a620dc33021c
kweku_annan <kwekuannan.dev@gmail.com>
2024-02-21 02:26:55 +0000

    Add 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Contains a function that reads a text file"""
+def read_file(filename=""):
+    """Reads a text file in a UTF8 encoding and prints content in the
+    standard output.
+    Args:
+        filename: Name of file to open. It can also be path of the file
+
+    Returns: Nothing
+    """
+    if filename == "" or filename == None:
+        return
+    with open(filename, 'r', encoding='utf-8') as a_file:
+        print(a_file.read(), end="")
