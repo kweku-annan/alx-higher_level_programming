f921472d74e6ecd6a223db74d991123a12c4d0e9
kweku_annan <kwekuannan.dev@gmail.com>
2024-02-22 04:15:03 +0000

    Add 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Contains a function that returns the dictionary description with simple
+data structure of an object"""
+import json
+
+
+def class_to_json(obj):
+    """Returns the dictionary representation of obj which is an
+    object of a class"""
+    return (obj.__dict__)
