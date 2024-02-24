630b3cc6bf29d4636bfed97833d21c7a072e10ea
kweku_annan <kwekuannan.dev@gmail.com>
2024-02-24 13:51:51 +0000

    Add 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""An empty Geometry Class with a public instance method"""
+
+
+class BaseGeometry:
+    """A base Geometry Class with a public instance method"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
