b71a65c6e523f9e496ff92812e349ff837146876
kweku_annan <kwekuannan.dev@gmail.com>
2024-02-24 10:25:39 +0000

    Add 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Checks if a specified object is an instance of a specified class"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if obj is an instance of a_class
+
+    Args:
+        obj:
+            Object to check class of.
+        a_class:
+            Class to check if obj is an instance of
+
+    Returns:
+       True: If obj is an instance of a_class
+       False: if obj is not an instance of a_class
+    """
+
+    return (isinstance(obj, a_class))
