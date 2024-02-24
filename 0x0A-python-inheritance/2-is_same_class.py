4d8717a51538ba0970b63c171175162c62bbcf04
kweku_annan <kwekuannan.dev@gmail.com>
2024-02-24 10:49:52 +0000

    Error check

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -16,7 +16,7 @@ def is_same_class(obj, a_class):
        False: if obj is not an instance of a_class
     """
 
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return (True)
     else:
         return (False)
