554f536bc11bb76fa1ec481cec86f3e94352e6f0
kweku_annan <kwekuannan.dev@gmail.com>
2024-03-29 05:42:03 +0000

    Add changes

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,7 @@
--- Creates the database hbtn_0d_usa and the table states
+-- Creates the database hbtn_0d_usa and the table cities
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT NOT NULL UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS states (
+id INT NOT NULL AUTO_INCREMENT,
+PRIMARY KEY (id), name VARCHAR(256) NOT NULL);
