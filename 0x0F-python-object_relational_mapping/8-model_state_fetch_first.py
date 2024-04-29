d6c51d3294765b517e57e4fe891d1fceba55f905
kweku_annan <kwekuannan.dev@gmail.com>
2024-04-29 03:35:23 +0000

    Add 8-model_state_fetch_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Lists all states objects from the database
+hbtn_0e_6_usa using SQLAlchemy (ORM)
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+
+    args = sys.argv[1:]
+
+    username = args[0]
+    password = args[1]
+    db_name = args[2]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, db_name
+        ),
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).order_by(State.id).first()
+    if result is None:
+        print()
+    else:
+        print("{}: {}".format(result.id, result.name))
