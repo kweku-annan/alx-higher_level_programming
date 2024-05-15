e5659ec584f7333206c2f661ae90fd36d323d7a7
kweku_annan <kwekuannan.dev@gmail.com>
2024-05-15 12:06:01 +0000

    Add changes

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = {
-        'to': email
+        'email': email
         }
     response = requests.post(url, data=data)
-    print("Your email is: {}".format(email))
+    print(response.content)
