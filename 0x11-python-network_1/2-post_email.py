130a3c122dc2bc772abe29e253a6917bfc5fb13f
kweku_annan <kwekuannan.dev@gmail.com>
2024-05-15 11:22:39 +0000

    Add changes

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     email_data = {
-        'to': email
+        'email': email
         }
     data = urllib.parse.urlencode(email_data)
     data = data.encode('ascii')
