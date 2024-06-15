9268a1eff289fa54fd42a83ac63709d33e5c57d0
kweku_annan <kwekuannan.dev@gmail.com>
2024-06-15 19:20:10 +0000

    Changes

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,9 +10,16 @@ def text_indentation(text):
     i = 0
     for char in text.split(" "):
         i += 1
+        punct_chars = ['.', ':', '?']
         if '.' in char or ':' in char or '?' in char:
-            print(char, end="")
-            print("\n\n", end="")
+            word_lst = list(char)
+            for letter in word_lst:
+                if letter in punct_chars:
+                    letter_idx = word_lst.index(letter)
+                    word_lst.insert(letter_idx + 1, '\n')
+                    word_lst.insert(letter_idx + 1, '\n')
+            new_char = ''.join(word_lst)
+            print(new_char)
         else:
             if len(text.split(" ")) == i:
                 print(char, end="")
